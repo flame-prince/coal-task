f932bedc1cf5229e266a226a4e9c1bc4457ce19b
salanemech <shallom.alanemech@vericast.com>
2024-03-12 01:20:13 -0400

    ..

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -57,48 +57,6 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-# Security Groups
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web"
-  description = "Allow web inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "AllowWebInbound"
-  }
-}
-
-resource "aws_security_group" "default" {
-  name        = "default_security_group"
-  description = "Default security group that allows no inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "DefaultSecurityGroup"
-  }
-}
-
 
 resource "aws_eip" "nat" {
   vpc = "true"
